1787fe65a8ebab3080e45105e407c02ffb6f875f
Anas Rih <anas.rih@gmail.com>
2026-09-25 06:49:31 +0000

    Add _meta.lua

---
+++ _meta.lua
@@ -0,0 +1,5 @@
+return {
+    name = "cloudlibsync",
+    fullname = "Cloud Lib Sync",
+    description = [[Syncs your KOReader library with a WebDAV server: automatic, bidirectional sync across multiple devices (Kindle, Kobo, Android...), with configurable sync direction and deletion propagation.]],
+}
